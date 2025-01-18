82f5ecf511fa2c0bb0b19268e6ee8412ed14221a
Drew Cook <drew@dco.dev>
2025-01-18 12:03:22 -0700

    feat: add unique constraint on account address

--- src/schema/entities/account.entity.ts
+++ src/schema/entities/account.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, Unique } from 'typeorm'
 import { Address } from 'viem'
 
 import { BaseEntity } from './base.entity'
 
+@Unique('UniqueAccountAddress', ['address'])
 @Entity('accounts')
 export class AccountEntity extends BaseEntity<AccountEntity> {
 	@Column()
